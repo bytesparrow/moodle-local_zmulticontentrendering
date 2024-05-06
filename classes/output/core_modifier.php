bae70d73e551374f11a0e7423072fedf1d76a48d
bytesparrow <5909549+bytesparrow@users.noreply.github.com>
2024-05-06 11:14:58 +0200

    add rest of the main content to the template

--- classes/output/core_modifier.php
+++ classes/output/core_modifier.php
@@ -41,6 +41,8 @@ class core_modifier extends \core_modifier_base {
     $model->basecontent = $this->extractBaseContent($maincontent);
     $model->pdfcontent = $this->extractPDF($maincontent);
 
+    $main_content_without_defined = str_replace([$model->basecontent, $model->pdfcontent], "", $maincontent);
+    $model->restcontent = $main_content_without_defined;
     //the template does the logic. If no PDF is present, it will only print one column, not two.
     $maincontent = $OUTPUT->render_from_template('local_zmulticontentrendering/wrapped_content', $model);
   }
